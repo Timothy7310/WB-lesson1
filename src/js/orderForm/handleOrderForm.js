256373d138998eebab1ab250cd90b6d1feeb9fdb
Timothy7310 <penlist127@gmail.com>
2023-08-09 15:05:57 +0300

    feat: add tooltip for order section

--- src/js/orderForm/handleOrderForm.js
+++ src/js/orderForm/handleOrderForm.js
@@ -1,7 +1,14 @@
+import { openTooltip } from "./openTooltip";
+
 export const handleOrderForm = () => {
   const orderForm = document.querySelector("#order-form");
+  const orderDeliveryTooltip = document.querySelector(
+    "#order-delivery-tooltip"
+  );
 
   orderForm.addEventListener("submit", (e) => {
     e.preventDefault();
   });
+
+  orderDeliveryTooltip.addEventListener("click", openTooltip);
 };
